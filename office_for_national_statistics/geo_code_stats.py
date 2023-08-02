0942c689849d3d46825f9fdd7eb472b9be7d8dfa
PinkRabbit <pink.rabbit.lzb@gmail.com>
2023-08-02 10:33:37 +0800

    refactor: 重构数据获取方法

--- office_for_national_statistics/geo_code_stats.py
+++ office_for_national_statistics/geo_code_stats.py
@@ -1,4 +1,4 @@
-__all__ = ["get_province_url"]
+__all__ = ["get_page_element"]
 
 import random
 import requests
@@ -6,10 +6,9 @@ from bs4 import BeautifulSoup
 from utils.web_scraping_tools import user_agent_list
 
 
-def get_province_url(year: int) -> dict:
+def get_page_element(url: str) -> dict:
     """获取省份用于跳转的URL
-
-    :param year: 整数类型 -> 年份，填入需要获取的那一年 -> 必需
+    :param url: 字符串类型 -> 网页链接 -> 必需
     :return province_url: 字典类型 -> key值为省份，value值为链接
     """
     headers = {
@@ -18,22 +17,32 @@ def get_province_url(year: int) -> dict:
     }
 
     response = requests.get(
-        url=f"http://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/{year}/",
+        url=url,
         headers=headers,
         verify=False
     )
-
     if response.status_code != 200:
         raise ConnectionError(f"数据获取错误，状态码为：{response.status_code}")
-
     html_parser = BeautifulSoup(response.content.decode('gbk'), 'html.parser')
 
-    city_url = "http://www.stats.gov.cn/sj/tjbz/tjyqhdmhcxhfdm/{year}/{html_name}"
+    if url.strip("/")[-4:] != "html":
+        page_element = {
+            i.get_text(): {
+                "code": i.find('a').attrs['href'].strip(".html") + "0000000000" if i.find('a') else None,
+                "next_level_url": url + i.find('a').attrs['href'] if i.find('a') else None
+            } for x in html_parser.select('.provincetr') for i in x.findAll('td')
+        }
+        return page_element
 
-    province_url = {
-        i.get_text(): city_url.format(year=year, html_name=i.find('a').attrs['href'] if i.find('a') else None)
-        for x in html_parser.select('.provincetr') for i in x.findAll('td')
-    }
+    elif url.strip("/")[-4:] == "html":
+        next_level_url = url.replace(url.split("/")[-1], "{href}")
+        page_element = {
+            x.findAll('td')[1].get_text(): {
+                "code": x.findAll('td')[0].get_text(),
+                "next_level_url": next_level_url.format(
+                    href=x.findAll('td')[1].find('a').attrs['href'] if x.findAll('td')[1].find('a') else None
+                )
+            } for x in html_parser.select('.citytr')
+        }
 
-    return province_url
-
+        return page_element
