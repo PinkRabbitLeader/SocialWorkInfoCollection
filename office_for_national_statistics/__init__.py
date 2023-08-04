fac41957df8ff33efedccb0789245c5d0b117b94
PinkRabbit <pink.rabbit.lzb@gmail.com>
2023-08-04 12:07:25 +0800

    style: 修改init文件

--- office_for_national_statistics/__init__.py
+++ office_for_national_statistics/__init__.py
@@ -1,5 +1,5 @@
-from .geo_code_stats import get_all_code
+from .geo_code_stats import get_all_code, multithreading_get_all_code
 
 __author__ = 'lzb'
-__version__ = (0, 0, 1)
-version = '0.0.1'
+__version__ = (0, 0, 2)
+version = '0.0.2'
